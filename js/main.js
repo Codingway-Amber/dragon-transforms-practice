54d34b3780a7643a0efba7a2e985ba649213ee0d
Codingway-Amber <amberreine7@gmail.com>
2024-10-24 10:53:15 -0500

    click to fade out

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-// JS scripts placed here
+// <--This will allow a user to click "game start" to fade it out -->
+const element = document.getElementById('enter-button');
+const intro = document.getElementById('intro');
+
+element.addEventListener('click', () => {
+    intro.classList.add('fade-out');
+});
